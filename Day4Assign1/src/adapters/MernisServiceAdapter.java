809d7a735fa17d6b777dfadfc0943a14f12a91f3
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-05-06 21:38:37 +0300

    Example Deleted

--- Day4Assign1/src/adapters/MernisServiceAdapter.java
+++ Day4Assign1/src/adapters/MernisServiceAdapter.java
@@ -8,28 +8,22 @@ import tr.gov.nvi.tckimlik.WS.KPSPublicSoapProxy;
 public class MernisServiceAdapter implements CustomerCheckService {
 
 	public boolean checkIfRealPerson(Customer customer) {
-		
+
 		KPSPublicSoap mernisValidation = new KPSPublicSoapProxy();
-		//mernisValidation.TCKimlikNoDogrula(Long TC, AD, SOYAD, Doðum Yýlý);
+
 		try {
-			mernisValidation.TCKimlikNoDogrula(Long.parseLong("12345678900"), "Salih", "Deðirmenci", 1999);
-		} catch (Exception e) {
-			
-		}
-		
-		try {
-			
-			if (mernisValidation.TCKimlikNoDogrula(Long.parseLong(customer.getNationalityId()),
-					customer.getFirstName(), customer.getLastName(), customer.getYearOfBirth())) {
+
+			if (mernisValidation.TCKimlikNoDogrula(Long.parseLong(customer.getNationalityId()), customer.getFirstName(),
+					customer.getLastName(), customer.getYearOfBirth())) {
 				return true;
-			}else {
+			} else {
 				return false;
 			}
-			
+
 		} catch (Exception e) {
 			return false;
 		}
-		
+
 	}
-	
+
 }
