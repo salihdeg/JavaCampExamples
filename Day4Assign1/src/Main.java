1189be8f367b6cc6f8e06acad2e987ebf1f88cf2
Salih Değirmenci <salihdegirmenci99@gmail.com>
2021-05-14 18:00:23 +0300

    New jars added

--- Day4Assign1/src/Main.java
+++ Day4Assign1/src/Main.java
@@ -1,13 +1,12 @@
 import abstracts.BaseCustomerManager;
 import adapters.MernisServiceAdapter;
-import concrete.NeroCustomerManager;
 import concrete.StarbucksCustomerManager;
 import entities.Customer;
 
 public class Main {
 
 	public static void main(String[] args) {
-		Customer salih = new Customer(1,"Salih","Deðirmenci",1999,"00000000000");
+		Customer salih = new Customer(1, "Salih", "Deðirmenci", 1999, "00000000000");
 
 		BaseCustomerManager customerManager = new StarbucksCustomerManager(new MernisServiceAdapter());
 
